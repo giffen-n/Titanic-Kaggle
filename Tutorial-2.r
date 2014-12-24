5ffb41934413b5ddff91aad1600f570bb45df376
unknown <NIGF@LC006050.nzww.nzcorp.net>
2014-12-24 00:47:09 -0500

    2nd Titanic Tutorial

---
+++ Tutorial-2.r
@@ -0,0 +1,8 @@
+# Nicholas Giffen - 23 Dec 2014
+# Titanic Tutorial 2 - by Trevor Stephens
+# Full guide available at: http://trevorstephens.com/
+
+# Set Working Directory and load data sets
+setwd("~/Titanic-Kaggle") 
+train <- read.csv("~/Titanic-Kaggle/train.csv") 
+test  <- read.csv("~/Titanic-Kaggle/test.csv")
