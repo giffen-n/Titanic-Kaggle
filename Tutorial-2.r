00231dfbc78cd3d642514597ad7240c9a9014cc8
unknown <NIGF@LC006050.nzww.nzcorp.net>
2014-12-24 01:06:20 -0500

    Tutorial 2

--- Tutorial-2.r
+++ Tutorial-2.r
@@ -2,7 +2,12 @@
 # Titanic Tutorial 2 - by Trevor Stephens
 # Full guide available at: http://trevorstephens.com/
 
-# Set Working Directory and load data sets - test comments
+# Set Working Directory and load data sets
 setwd("~/Titanic-Kaggle") 
 train <- read.csv("~/Titanic-Kaggle/train.csv") 
 test  <- read.csv("~/Titanic-Kaggle/test.csv")
+
+# View number of females and males and proportion of each that survived
+summary(train$Sex)
+prop.table(table(train$Sex, train$Survived))
+prop.table(table(train$Sex, train$Survived), 1)
