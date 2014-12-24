5dc3964f9fcc00560ddc22c87fc9582bf5fc6149
unknown <NIGF@LC006050.nzww.nzcorp.net>
2014-12-24 10:55:12 -0500

    Titanic Tutorial #2

--- Tutorial-2.r
+++ Tutorial-2.r
@@ -31,3 +31,21 @@ train$Child[train$Age < 18] <- 1
 aggregate(Survived ~ Child + Sex, data=train, FUN=sum)
 aggregate(Survived ~ Child + Sex, data=train, FUN=length)
 aggregate(Survived ~ Child + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
+
+# Bin fares into 4 groups, store as new column Fare2 in Training set and observe patterns
+train$Fare2 <- '30+'
+train$Fare2[train$Fare < 30 & train$Fare >= 20] <- '20-30'
+train$Fare2[train$Fare < 20 & train$Fare >= 10] <- '10-20'
+train$Fare2[train$Fare < 10] <- '<10'
+aggregate(Survived ~ Fare2 + Pclass + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
+
+# Create new column in test set with our prediction that everyone dies
+test$Survived <- 0
+# Update the prediction to say that all females will survive
+test$Survived[test$Sex == 'female'] <- 1
+# Update once more to say that females who pay more for a third class fare also perish
+test$Survived[test$Sex == 'female' & test$Pclass == 3 & test$Fare >= 20] <- 0
+
+# Create submission dataframe and output to file
+submit <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
+write.csv(submit, file = "genderclassmodel.csv", row.names = FALSE)
